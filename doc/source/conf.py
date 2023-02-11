0f52f4187211aca81760ff2a9220dd221aeeae6c
Greg <greg@elphick.com.au>
2023-02-11 12:58:46 +0800

    widened the doc page width

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -69,9 +69,13 @@ exclude_patterns = ['_build', '_templates']
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
-# html_style = '_static/my_theme.css'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# to widen the page...
+html_css_files = [
+    'custom.css',
+]
