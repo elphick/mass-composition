eb87149e0592ca22dbdb7e71fb5bce0a62bbdec0
Greg <greg@elphick.com.au>
2023-02-17 17:43:05 +0800

    added spatial columns to sample data

--- sample_data/iron_ore_sample_data_wrangle.py
+++ sample_data/iron_ore_sample_data_wrangle.py
@@ -16,7 +16,7 @@ import pandas as pd
 # -----------------------------------------------------
 #
 # We get some demo data in the form of a pandas DataFrame
-from elphick.mc.mass_composition.utils.components import is_compositional
+from elphick.mass_composition import is_compositional
 
 df_data: pd.DataFrame = pd.read_csv('A072391_met.csv')
 df_data.dropna(inplace=True)
@@ -33,10 +33,31 @@ df_data.rename(
     inplace=True)
 
 extra_cols: List[str] = ['DHID', 'interval_from', 'interval_to']
-mass_comp_data: pd.DataFrame = df_data[['mass_dry', 'H2O'] + cols_components + extra_cols]
+mass_comp_data: pd.DataFrame = df_data[['mass_dry', 'H2O'] + cols_components + extra_cols].copy()
 mass_comp_data.sort_values(['DHID', 'interval_from'], inplace=True)
 mass_comp_data.index.name = 'index'
 
 mass_comp_data.to_csv('../sample_data/iron_ore_sample_data_A072391.csv')
 
-print('done')
+# %%
+#
+# Wrangle to create 3D data
+
+df_collars: pd.DataFrame = pd.read_csv('A072391_collars.csv')
+df_collars.dropna(how='all', axis='index', inplace=True)
+df_collars.drop(columns='Unnamed: 0', inplace=True)
+
+# add the collar data to the mass_comp_data data
+# The DHID does not match across the datasets - we'll make an assumption - it may be wrong!
+df_collars['DHID'] = df_collars['HOLEID'].apply(lambda x: x[0:2] + 'S' + x[-2::])
+
+df_res: pd.DataFrame = pd.merge(left=mass_comp_data, right=df_collars.iloc[0: 99][['DHID', 'EAST', 'NORTH', 'RL']],
+                                left_on='DHID', right_on='DHID')
+df_res.rename(columns={'EAST': 'x', 'NORTH': 'y'}, inplace=True)
+df_res['z_lo'] = df_res['RL'] - df_res['interval_from']
+df_res['z_hi'] = df_res['RL'] - df_res['interval_to']
+df_res['z'] = df_res[['z_lo', 'z_hi']].mean(axis='columns')
+df_res.drop(columns=['z_lo', 'z_hi', 'RL'], inplace=True)
+df_res.index.name = 'index'
+
+df_res.to_csv('../sample_data/iron_ore_sample_data_xyz_A072391.csv')
