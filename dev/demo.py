abcb586be53e8db1aeb85c57fac07edb390cd7f2
Greg <greg@elphick.com.au>
2023-03-18 22:24:20 +0800

    commiting deletion of dev content - moved to develop branch

--- dev/demo.py
+++
@@ -1,9 +0,0 @@
-from flowsheet import Stream
-from test.data.sample_data import sample_data
-
-
-obj_strm: Stream = Stream.from_dataframe(data=sample_data(), name='my_name')
-
-print(obj_strm.name, '\n', obj_strm._obj)
-
-print('done')
