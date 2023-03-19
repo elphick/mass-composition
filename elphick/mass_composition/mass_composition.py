f5dc44d2949646b18e57df503cd6ddd4cabba458
Greg <greg@elphick.com.au>
2023-03-19 12:18:32 +0800

    changed .yaml to .yml

--- elphick/mass_composition/mass_composition.py
+++ elphick/mass_composition/mass_composition.py
@@ -43,7 +43,7 @@ class MassComposition:
         var_args: Dict = {k: v for k, v in locals().items() if '_var' in k}
 
         if config_file is None:
-            config_file = Path(__file__).parent / './config/mc_config.yaml'
+            config_file = Path(__file__).parent / './config/mc_config.yml'
         self.config = read_yaml(config_file)
 
         # if interval pairs are passed as indexes then create the proper interval index
