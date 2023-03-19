479fe1184e06d52d4a80552dfc30e05821d2bf7a
Greg <greg@elphick.com.au>
2023-03-19 12:19:12 +0800

    changed .yaml to .yml, added specific local test config files

--- test/test_config.py
+++ test/test_config.py
@@ -4,9 +4,9 @@ from elphick.mass_composition.mass_composition import read_yaml
 
 
 def test_config():
-    read_yaml('../elphick/mass_composition\config\mc_config.yaml')
+    read_yaml('config/test_mc_config.yml')
 
 
 def test_not_config():
     with pytest.raises(KeyError):
-        read_yaml('not_mc_config.yaml')
+        read_yaml('config/not_mc_config.yml')
