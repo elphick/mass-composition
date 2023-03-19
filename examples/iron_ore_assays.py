a983da1e311a4ff6e8f0abd4e43f5f2dd09c79a9
Greg <greg@elphick.com.au>
2023-03-19 15:06:25 +0800

    added missing name - again

--- examples/iron_ore_assays.py
+++ examples/iron_ore_assays.py
@@ -15,7 +15,6 @@ from plotly.graph_objs import Figure
 
 from elphick.mass_composition import MassComposition
 
-
 # %%
 #
 # Create a MassComposition object
@@ -23,6 +22,7 @@ from elphick.mass_composition import MassComposition
 # We get some demo data in the form of a pandas DataFrame
 
 df_data: pd.DataFrame = iron_ore_sample_data()
+name = 'A072391'
 
 print(df_data.shape)
 df_data.head()
@@ -30,7 +30,7 @@ df_data.head()
 # %%
 # ...and create a MassComposition from the DataFrame.
 
-obj_mc: MassComposition = MassComposition(df_data, name='A072391')
+obj_mc: MassComposition = MassComposition(df_data, name=name)
 obj_mc.aggregate(group_var='DHID')
 
 # %%
@@ -38,7 +38,8 @@ obj_mc.aggregate(group_var='DHID')
 # Parallel plots
 # --------------
 
-obj_mc: MassComposition = MassComposition(df_data.reset_index().set_index(['DHID', 'interval_from', 'interval_to']), name=name)
+obj_mc: MassComposition = MassComposition(df_data.reset_index().set_index(['DHID', 'interval_from', 'interval_to']),
+                                          name=name)
 
 fig: Figure = obj_mc.plot_parallel(color='Fe')
 fig
@@ -54,4 +55,3 @@ fig
 fig: Figure = obj_mc.plot_parallel(color='Fe', var_subset=['mass_wet', 'H2O', 'Fe', 'SiO2'])
 # noinspection PyTypeChecker
 plotly.io.show(fig)  # this call to show will set the thumbnail for the gallery
-
