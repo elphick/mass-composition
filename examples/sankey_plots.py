bd8f96482ce23856d4beb5541fab1ad982764ca9
Greg Elphick <Elphick@users.noreply.github.com>
2023-03-26 19:55:44 +0800

    Update sankey_plots.py
    sankey thumnail #2

--- examples/sankey_plots.py
+++ examples/sankey_plots.py
@@ -7,12 +7,15 @@ Related MassComposition objects are managed as a network.
 """
 
 import pandas as pd
+import matplotlib.pyplot as plt
 import plotly
 from plotly.graph_objs import Figure
 
 from elphick.mass_composition import MassComposition
 from elphick.mass_composition.mc_network import MCNetwork
 from test.data import sample_data
+
+sphinx_gallery_thumbnail_number = 2
 
 # %%
 #
@@ -35,7 +38,7 @@ obj_mc_1, obj_mc_2 = obj_mc.split(0.4)
 mcn: MCNetwork = MCNetwork().from_streams([obj_mc, obj_mc_1, obj_mc_2])
 
 hf = mcn.plot_network()
-hf;
+plt.show()
 
 # %%
 #
@@ -47,8 +50,7 @@ hf;
 # In this example, grades are the same, so will not add any value.
 
 fig: Figure = mcn.plot_sankey()
-fig
 # noinspection PyTypeChecker
-# plotly.io.show(fig)  # this call to show will set the thumbnail for use in the gallery
+plotly.io.show(fig)  # this call to show will set the thumbnail for use in the gallery
 
 # %%
