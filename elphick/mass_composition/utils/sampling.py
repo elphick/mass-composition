5be82b50999ceaeb41a63d473ba2b8a7abd8030b
Greg <greg@elphick.com.au>
2023-04-10 20:39:38 +0800

    temporary solution to the sampling problem (was getting duplicate nodes) - need a better solution later.

---
+++ elphick/mass_composition/utils/sampling.py
@@ -0,0 +1,10 @@
+import random
+from typing import Final
+
+MAX_NODES: Final[int] = int(1e10)
+
+
+def random_int():
+    # ideally sample without replacement and return a single int
+    # sadly multiple calls mean it is not without replacement, managing temporarily with a large range
+    return random.sample(range(MAX_NODES), 1)[0]
