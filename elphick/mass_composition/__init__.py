2cde27611202523925af6bcc0266a4ff152985ea
Greg <11791585+elphick@users.noreply.github.com>
2023-06-22 06:21:08 +0800

    added __version__ linked to toml

--- elphick/mass_composition/__init__.py
+++ elphick/mass_composition/__init__.py
@@ -2,3 +2,6 @@ from __future__ import annotations
 
 from .mass_composition import MassComposition
 from .mc_xarray import MassCompositionAccessor
+
+import importlib.metadata
+__version__ = importlib.metadata.version('mass-composition')
