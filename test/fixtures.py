1d138a21f2a1c3c4dedc22beff3ebe6a5b24f9b0
Greg <11791585+elphick@users.noreply.github.com>
2023-08-03 07:25:38 +0800

    added example network object to fixtures.py

--- test/fixtures.py
+++ test/fixtures.py
@@ -1,7 +1,13 @@
+from functools import partial
+
 import pandas as pd
 import pytest
 
+from elphick.mass_composition import MassComposition
 from elphick.mass_composition.datasets.sample_data import sample_data, size_by_assay
+from elphick.mass_composition.network import MCNetwork
+from elphick.mass_composition.utils.partition import perfect
+
 
 @pytest.fixture
 def demo_data():
@@ -21,3 +27,14 @@ def demo_data_2():
 def size_assay_data():
     data: pd.DataFrame = size_by_assay()
     return data
+
+
+@pytest.fixture
+def demo_size_network(size_assay_data):
+    mc_size: MassComposition = MassComposition(size_assay_data, name='size sample')
+    partition = partial(perfect, d50=0.150, dim='size')
+    mc_coarse, mc_fine = mc_size.partition(definition=partition)
+    mc_coarse.name = 'coarse'
+    mc_fine.name = 'fine'
+    mcn: MCNetwork = MCNetwork().from_streams([mc_size, mc_coarse, mc_fine])
+    return mcn
