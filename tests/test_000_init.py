72b34151cedebbfc6fda34482418003505d5805b
Greg Elphick <11791585+elphick@users.noreply.github.com>
2024-05-12 20:04:20 +0800

    closes #107.  Added warning for MultiIndex > 2. (#145)

--- tests/test_000_init.py
+++ tests/test_000_init.py
@@ -1,3 +1,7 @@
+import logging
+
+import pandas as pd
+
 from elphick.mass_composition import MassComposition
 from .fixtures import demo_data
 
@@ -8,3 +12,19 @@ def test_init(demo_data):
 
     # Assert that the name attribute is correctly set
     assert obj_mc.name == 'test', "The name attribute was not correctly set during initialization"
+
+
+def test_multiindex_warning(demo_data, caplog):
+    # set a 3 level index on our demo dataframe
+    index = pd.MultiIndex.from_tuples([(1, 2, 3), (4, 5, 6), (7, 8, 9)], names=['x', 'y', 'z'])
+    demo_data.set_index(index, inplace=True)
+
+    # Create a MassComposition object
+    mc = MassComposition()
+
+    with caplog.at_level(logging.WARNING):
+        # Pass the DataFrame to the set_data method
+        mc.set_data(demo_data)
+
+    # Check if a warning was logged
+    assert "The data has more than 2 levels in the index" in caplog.text
