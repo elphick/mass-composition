61e06e22e3b1cd58f9bf6d1dfaa9fa52f1a65335
Greg <11791585+elphick@users.noreply.github.com>
2024-05-18 23:45:25 +0800

    closes #171. DAG example with estimation. fixed failing tests.

--- elphick/mass_composition/mass_composition.py
+++ elphick/mass_composition/mass_composition.py
@@ -125,9 +125,9 @@ class MassComposition:
             self._data = xr_ds
 
             if not self._name:
-                self.rename(common_prefix.strip('_'))
+                self.rename('unnamed') if not common_prefix else self.rename(common_prefix.strip('_'))
             else:
-                self.rename('unnamed') if not self._name else self.rename(self._name)
+                self.rename(self._name)
 
         # explicitly define the constraints
         self.constraints: Dict = self.get_constraint_bounds(constraints=constraints)
